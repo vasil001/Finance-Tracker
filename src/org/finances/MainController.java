058fdfa6e021e5328259a8cee6f5a7f6f6df1e7a
Vasili Ntiso <v.n1996@hotmail.com>
2020-05-06 20:49:12 +0200

    Show Graphs Button reads data from DB and visualizes it.

--- src/org/finances/MainController.java
+++ src/org/finances/MainController.java
@@ -8,19 +8,17 @@ package org.finances;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -31,11 +29,17 @@ import javafx.stage.Stage;
  */
 public class MainController implements Initializable {
     
-   
+
     @FXML
     private Button btnFinances;
     @FXML
     private Label label;
+    @FXML
+    private Button btnShowGraphs;
+    @FXML
+    private LineChart<String, Integer> linechart;
+    
+ 
     
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
@@ -44,8 +48,9 @@ public class MainController implements Initializable {
             Stage stage = new Stage();
             Scene scene = new Scene(part);
             stage.setScene(scene);
-            stage.show();
-              
+            stage.show();     
+          }else if(event.getSource() == btnShowGraphs){
+              showGraphs();
           }
     }
     
@@ -53,6 +58,26 @@ public class MainController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
     }   
-    
-    
+        
+    public void showGraphs(){
+        linechart.getData().clear();
+        String query = "SELECT amount, date FROM myData.finances ORDER BY date";
+        FinancesController f = new FinancesController();
+        Connection c = f.getConnection(); 
+
+         try{
+            ResultSet rs = c.createStatement().executeQuery(query);
+            XYChart.Series<String, Integer>  series = new XYChart.Series<>();
+            
+            while(rs.next()){   // If these is sth. on the list then do:
+                series.getData().add(new XYChart.Data<>(rs.getString(2),rs.getInt(1)));
+            }
+            
+            linechart.getData().add(series);
+            
+        }catch(SQLException ex){
+            System.out.println("ERROR getFinancesList(): " + ex.getMessage());
+
+        }
+    }  
 }
